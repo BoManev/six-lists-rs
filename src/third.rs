e50c62f788d7041d48c0401624b5addab0672214
BoManev <bomanev@pm.me>
2023-08-07 17:26:55 -0400

    basic persistent immutable list

---
+++ src/third.rs
@@ -0,0 +1,80 @@
+use std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+pub struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn prepend(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem: elem,
+                next: self.head.clone(),
+            })),
+        }
+    }
+
+    pub fn tail(&self) -> List<T> {
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let list: List<u32> = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.prepend(1).prepend(2).prepend(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+    }
+
+    #[test]
+    fn basics1() {
+        let list: List<u32> = List::new();
+        let list1 = list.prepend(1).prepend(2).prepend(3);
+
+        let list2 = list1.tail();
+        let list2 = list2.prepend(4);
+        assert_eq!(list2.head(), Some(&4));
+        assert_eq!(list1.head(), Some(&3));
+
+        let list3 = list1.prepend(5);
+        assert_eq!(list3.head(), Some(&5));
+        assert_eq!(list1.head(), Some(&3));
+
+    }
+}
+
